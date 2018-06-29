2e3fdf710cd41c9833a636c7fdb9c375d75f2f73
Karim Kawambwa <karimkawambwa@icloud.com>
2018-06-29 13:50:55 -0400

    fixed test and added route

--- src/main/java/com/swoop/devtest/MathController.java
+++ src/main/java/com/swoop/devtest/MathController.java
@@ -1,16 +1,31 @@
 package com.swoop.devtest;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import com.swoop.devtest.Result;
 
 @RestController
+@RequestMapping("/math")
 public class MathController {
 
   @RequestMapping("/add")
-  public Result add(@RequestParam(name="n1") double n1, @RequestParam(name="n2") double n2) {
+  public Result getAdd(@RequestParam(name="n1") double n1, @RequestParam(name="n2") double n2) {
     return new Result(n1 + n2);
   }
+
+  @RequestMapping(value="/add", 
+  method = RequestMethod.POST,
+  consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  public Result postAdd(@RequestBody MultiValueMap<String, String> formData) {
+    System.out.println("Creating Article in controller. MultiValueMap= " + formData);
+    return new Result(Double.parseDouble(formData.getFirst("n1"))+ Double.parseDouble(formData.getFirst("n2")));
+  }
+
  }
--- src/test/java/com/swoop/devtest/DemoApplicationTests.java
+++ src/test/java/com/swoop/devtest/DemoApplicationTests.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,17 +31,25 @@ public class DemoApplicationTests {
 	 */
 	@Test
 	public void onePlusOneEqualsTwo() throws Exception {
-		this.mockMvc.perform(get("/math/add?n1=1&n2=1")).andExpect(status().isOk()).andExpect(jsonPath("$.result").value(2));
+		this.mockMvc.perform(get("/math/add?n1=1&n2=1")).andExpect(status().isOk())
+				.andExpect(jsonPath("$.result").value(2));
 	}
 
 	/*
 	 * POST : http://<server_url>/math/add (allow for form params 1&2 in a POST
 	 * body) This should add two numbers from a POST body
 	 */
+	@Test
 	public void postOnePlusOneEqualsTwo() throws Exception {
-		this.mockMvc.perform(post("/math/add", 1,1)).andExpect(status().isOk()).andExpect(jsonPath("$.result").value(2));
+		this.mockMvc
+				.perform(post("/math/add")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("n1", "1")
+				.param("n2", "1"))
+				.andDo(print())
+				.andExpect(status().isOk()).andExpect(jsonPath("$.result").value(2));
 	}
-	
+
 	/*
 	 * GET : http://<server_url>/time/now
 	 * 
